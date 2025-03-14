963f21db36715361816637a63bfc16b369137bd7
ZachVFX <zachallrd@gmail.com>
2025-03-15 00:43:27 +0100

    qsd

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ from protocols import Request, Response
 
 
 class Client:
-    def __init__(self, host="127.0.0.1", port=55555):
+    def __init__(self, host="192.168.159.146", port=55555):
         self.nickname = None
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.connect((host, port))
